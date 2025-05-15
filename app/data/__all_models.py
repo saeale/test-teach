e38f6dcbfe1852573d2eff7c6143ae1a2c118258
saeale <8919513s@gmail.com>
2025-05-16 02:53:20 +0300

    Delete app directory

--- app/data/__all_models.py
+++
@@ -1,9 +0,0 @@
-from . import users
-from . import news
-from . import category
-
-from . import students
-from . import teachers
-from . import tests
-from . import attempts
-from . import questions
--- app/forms/add_teacher.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
-
-
-class TeacherAddForm(FlaskForm):
-    teacher_login = StringField("Введите логин учителя", validators=[DataRequired()])
-    add = SubmitField('Добавить')
-
